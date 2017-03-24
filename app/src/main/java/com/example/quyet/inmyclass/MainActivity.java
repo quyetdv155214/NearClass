acb3336e318bcd200ac643e0a55cc7e230d9f702
DESKTOP-UPT3L84\quyet <quyetdv155214@gmail.com>
2017-03-24 20:34:32 +0700

    them thu vien va api key

--- app/src/main/java/com/example/quyet/inmyclass/MainActivity.java
+++ app/src/main/java/com/example/quyet/inmyclass/MainActivity.java
@@ -3,11 +3,20 @@ package com.example.quyet.inmyclass;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.nearby.Nearby;
+
 public class MainActivity extends AppCompatActivity {
 
+    GoogleApiClient mGoogleApiClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Nearby.MESSAGES_API)
+                    .addConnectionCallbacks((GoogleApiClient.ConnectionCallbacks) this)
+                    .enableAutoManage(this, (GoogleApiClient.OnConnectionFailedListener) this)
+                    .build();
     }
 }
