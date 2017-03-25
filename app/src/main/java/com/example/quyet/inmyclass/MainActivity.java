26e6be4f119310a799eb7da0362fcd4ea9765625
DESKTOP-UPT3L84\quyet <quyetdv155214@gmail.com>
2017-03-26 05:08:17 +0700

    chay thu

--- app/src/main/java/com/example/quyet/inmyclass/MainActivity.java
+++ app/src/main/java/com/example/quyet/inmyclass/MainActivity.java
@@ -1,22 +1,66 @@
 package com.example.quyet.inmyclass;
 
+import android.content.IntentSender;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Switch;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.nearby.Nearby;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
+    private static final String TAG = "MainActivity";
+    @BindView(R.id.sw_connected)
+    Switch swConnected;
+    @BindView(R.id.sw_dis_connected)
+    Switch swDisConnected;
 
     GoogleApiClient mGoogleApiClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addApi(Nearby.MESSAGES_API)
-                    .addConnectionCallbacks((GoogleApiClient.ConnectionCallbacks) this)
-                    .enableAutoManage(this, (GoogleApiClient.OnConnectionFailedListener) this)
-                    .build();
+        ButterKnife.bind(this);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Nearby.MESSAGES_API)
+                .addConnectionCallbacks(this)
+                .enableAutoManage(this, this)
+                .build();
     }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        swConnected.setChecked(true);
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        swConnected.setChecked(false);
+
+        Log.d(TAG, "GoogleApiClient disconnected with cause: " + cause);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult result) {
+        swDisConnected.setChecked(true);
+        if (result.hasResolution()) {
+            try {
+                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.e(TAG, "GoogleApiClient connection failed");
+        }
+    }
+
 }
